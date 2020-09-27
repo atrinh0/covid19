76b802f45d0fd7aeb8edec68c507cdd54c95df3d
An Trinh <atrinh@gmail.com>
2020-09-27 16:59:00 +0100

    Initial commit

---
+++ covid19/covid19/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  covid19
+//
+//  Created by An Trinh on 27/9/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ covid19/covid19/covid19App.swift
@@ -0,0 +1,17 @@
+//
+//  covid19App.swift
+//  covid19
+//
+//  Created by An Trinh on 27/9/20.
+//
+
+import SwiftUI
+
+@main
+struct covid19App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
