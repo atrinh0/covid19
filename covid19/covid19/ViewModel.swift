6da3298464a5c61956d1a416043d7453a1e4796b
An Trinh <atrinh@gmail.com>
2021-03-13 20:16:21 +0000

    Balance emphasised deaths data with widget

--- covid19/covid19/ViewModel.swift
+++ covid19/covid19/ViewModel.swift
@@ -182,7 +182,7 @@ class ViewModel: ObservableObject {
         let maxCasesScalingValue = (casesArray.max() ?? 1.0) * 1.05
         let maxDeathsScalingValue = (deathsArray.max() ?? 1.0) * 1.05
         casesData = casesArray.map { $0/maxCasesScalingValue }.reversed()
-        rawDeathsData = deathsArray.map { $0/maxDeathsScalingValue }.reversed()
+        rawDeathsData = deathsArray.map { $0/(maxDeathsScalingValue * 2)}.reversed()
         relativeDeathsData = deathsArray.map { $0/maxCasesScalingValue }.reversed()
     }
     
